16c242cbe1a480404c4e2d48c317ee6213a92342
Moisés Navarro <moisalejandro@gmail.com>
2020-11-10 12:47:06 -0600

    [IMP] simplify

--- project_development/models/project_task.py
+++ project_development/models/project_task.py
@@ -5,7 +5,6 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
 
     repo = fields.Char()
-    branch = fields.Char()
     pull_request = fields.Char()
     sprint_id = fields.Many2one(
         comodel_name="project.sprint",
